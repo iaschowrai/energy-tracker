e40737f608bae351bd238ae702703df2aacec0bc
iasch <iaschowrai@gmail.com>
2025-11-23 23:16:02 -0700

    created user

---
+++ user-service/src/main/java/com/iaschowrai/user_service/entity/User.java
@@ -0,0 +1,25 @@
+package com.iaschowrai.user_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String address;
+    private boolean alerting;
+    private double energyAlertingThreshold;
+}
--- user-service/src/main/java/com/iaschowrai/user_service/service/UserService.java
+++ user-service/src/main/java/com/iaschowrai/user_service/service/UserService.java
@@ -2,6 +2,8 @@ package com.iaschowrai.user_service.service;
 
 import com.iaschowrai.user_service.dto.UserDto;
 
+import com.iaschowrai.user_service.entity.User;
+import com.iaschowrai.user_service.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -11,9 +13,38 @@ public class UserService {
 
     private final Logger log = LoggerFactory.getLogger(UserService.class);
 
-    public UserDto createUser(UserDto userDto) {
+    private final UserRepository userRepository;
 
-        log.info("Creating users: {}", userDto);
-        return userDto;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserDto createUser(UserDto input) {
+
+        log.info("Creating users: {}", input);
+
+        final User createdUser = User.builder()
+                .name(input.getName())
+                .email(input.getEmail())
+                .address(input.getAddress())
+                .alerting(input.isAlerting())
+                .energyAlertingThreshold(input.getEnergyAlertingThreshold())
+                .build();
+
+        User saved =  userRepository.save(createdUser);
+
+        return toDto(saved);
+    }
+
+    private UserDto toDto(User user) {
+
+        return UserDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .address(user.getAddress())
+                .alerting(user.isAlerting())
+                .energyAlertingThreshold(user.getEnergyAlertingThreshold())
+                .build();
     }
 }
